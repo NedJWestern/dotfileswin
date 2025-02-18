ff01f53d51d00f85810ed3ae7174a1ba40904492
Ned Western <ned.western@gmail.com>
2025-02-18 17:15:25 +1100

    Create df-init.ps1

---
+++ df-init.ps1
@@ -0,0 +1,10 @@
+echo '. ~/profile.ps1' > ~/Documents/PowerShell/profile.ps1
+
+# prevent git recursion issues
+echo ".dotfiles" > $HOME/.gitignore
+
+# clone and setup git repo
+git clone --bare https://github.com/NedJWestern/dotfileswin.git $HOME/.dotfileswin
+git --git-dir=$HOME/.dotfileswin/ --work-tree=$HOME checkout
+git --git-dir=$HOME/.dotfileswin/ --work-tree=$HOME config --local status.showUntrackedFiles no
+
